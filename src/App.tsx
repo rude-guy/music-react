657cfcd0cd8538455d88d360cc93d9f26ae8f578
rude-guy <2064825358.@qq.com>
2022-01-11 21:34:59 +0800

    build: project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+    </div>
+  );
+}
+
+export default App;
