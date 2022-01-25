5a2aa5be47eda5b7462b9e0e746b82e921476447
rude-guy <2064825358.@qq.com>
2022-01-25 20:37:56 +0800

    refactor: 更换loading

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import styles from './Loading.module.css'
-import loading from './loading.gif'
 
 const Loading = React.memo(({title = '正在载入...'}: { title?: string }) => {
     return <div className={styles.loading}>
         <div className={styles.loadingContent}>
-            <img width={"24"} height={"24"} src={loading} alt={'loading'}/>
+            <div className={styles.container}>
+                <div className={`${styles.one} ${styles.common}`}/>
+                <div className={`${styles.two} ${styles.common}`}/>
+                <div className={`${styles.three} ${styles.common}`}/>
+                <div className={`${styles.four} ${styles.common}`}/>
+                <div className={`${styles.five} ${styles.common}`}/>
+                <div className={`${styles.six} ${styles.common}`}/>
+                <div className={`${styles.seven} ${styles.common}`}/>
+                <div className={`${styles.eight} ${styles.common}`}/>
+            </div>
             <p className={styles.desc}>{title}</p>
         </div>
     </div>
